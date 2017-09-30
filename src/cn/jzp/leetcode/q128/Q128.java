339c16b05d5c509a786ff8e89d754110ebc707bb
jiangzhipeng1 <jiangzhipeng1@jd.com>
2017-09-30 18:53:25 +0800

    Q128 notfinish

---
+++ src/cn/jzp/leetcode/q128/Q128.java
@@ -0,0 +1,36 @@
+package cn.jzp.leetcode.q128;
+
+import cn.jzp.leetcode.common.annotation.NotFinish;
+
+import java.util.*;
+
+/**
+ * Created by jiangzhipeng1 on 2017/9/30.
+ */
+@NotFinish
+public class Q128 {
+    
+    class Solution {
+        
+        public int longestConsecutive(int[] nums) {
+            Map<Integer,Integer> map = new HashMap<>();
+            Set<Integer> set = new TreeSet<>();
+            int maxDepth = 1;
+            for(int i:nums){
+                set.add(i);
+            }
+            for(int i:set){
+                if(!map.containsKey(i)){
+                    map.put(i,1);
+                }
+                if(set.contains(i+1)){
+                    int res = map.get(i)+1;
+                    map.put(i+1,res);
+                    if(res > maxDepth){
+                        maxDepth = res;
+                    }
+                }
+            }
+            return 0;
+    }
+}
